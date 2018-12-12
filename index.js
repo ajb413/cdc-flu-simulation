ebf0eb89ba8a0169fab9e79a6c5bfd8b64eb0295
Adam Bavosa <adamb@pubnub.com>
2018-12-12 12:40:58 -0800

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const PubNub = require('pubnub');
+const cities = require('./usCities.json').cities;
+const maxTimeout = 10000;
+const minTimeout = 4000;
+let randomMillisecondInterval = 0;
+
+const pubnub = new PubNub({
+    publishKey : 'pub-c-74a9f89f-039e-47ea-af97-7771cea4d927',
+    subscribeKey : 'sub-c-63af100c-fd9d-11e8-8ebf-6a684a5fb351'
+});
+
+function randomNumber(max, min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function main() {
+    // Random number is inclusive of the min and max
+    const max = cities.length - 1;
+    const min = 0;
+    const randomArrayIndex = randomNumber(max, min);
+
+    pubnub.publish({
+        channel : 'cdc-flu-instance-stream',
+        message: {
+            time: new Date().getTime().toString().substring(0, 10),
+            coordinates: cities[randomArrayIndex]
+        }
+    });
+
+    randomMillisecondInterval = randomNumber(maxTimeout, minTimeout);
+    console.log(randomMillisecondInterval);
+    setTimeout(main, randomMillisecondInterval);
+}
+
+setTimeout(main, randomMillisecondInterval);
